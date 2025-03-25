34e231b0d8c55e1c37c9ad3b27edf75c0e5ab6a8
David Guillen Fandos <david@davidgf.net>
2025-03-25 01:04:09 +0100

    More signals

--- main.zig
+++ main.zig
@@ -19,6 +19,19 @@ pub fn main() !void {
     // names.add_signal_name(Chip.pins._20.fb(), "Signal_Name");  // .fb() returns the raw macrocell output/feedback signal for the macrocell corresponding to the pin (i.e. enum names starting with `mc_`; note this isn't necessarily the same as the `io_` signal even for outputs, since ORM and OE apply to the latter)
 
     // Start adding known signal names
+    try names.add_signal_name(.io_A8,  "GP-0");
+    try names.add_signal_name(.io_A10, "GP-1");
+    try names.add_signal_name(.io_A12, "GP-2");
+    try names.add_signal_name(.io_A14, "GP-3");
+    try names.add_signal_name(.io_B0,  "GP-4");
+    try names.add_signal_name(.io_B2,  "GP-5");
+    try names.add_signal_name(.io_B4,  "GP-6");
+    try names.add_signal_name(.io_B6,  "GP-7");
+    try names.add_signal_name(.io_B8,  "GP-8");
+    try names.add_signal_name(.io_B10, "GP-9");
+    try names.add_signal_name(.io_B12, "GP-10");
+
+    try names.add_signal_name(.io_B14, "GP-18");
     try names.add_signal_name(.io_C5,  "GP-19");
     try names.add_signal_name(.io_C4,  "GP-20");
     try names.add_signal_name(.io_C2,  "GP-21");
