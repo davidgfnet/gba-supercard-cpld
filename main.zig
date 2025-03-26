fc186cc06dfab0629ddc4aeadbe5bcbce02c4ee3
David Guillen Fandos <david@davidgf.net>
2025-03-26 01:12:42 +0100

    Adding more signals

--- main.zig
+++ main.zig
@@ -18,8 +18,51 @@ pub fn main() !void {
     // names.add_signal_name(Chip.pins._20.pad(), "Signal_Name"); // .pad() returns the signal associated with the I/O cell corresponding to the pin (i.e. enum names starting with `clk` or `io_`)
     // names.add_signal_name(Chip.pins._20.fb(), "Signal_Name");  // .fb() returns the raw macrocell output/feedback signal for the macrocell corresponding to the pin (i.e. enum names starting with `mc_`; note this isn't necessarily the same as the `io_` signal even for outputs, since ORM and OE apply to the latter)
 
+
+    // davidgf:
+    // General overview
+    //
+    // The SRAM chip is mostly driven by the bus using 16 wires as addr and another 8 as data.
+    // The flash chip is driven by the CPLD (lower 16 bits).
+
+
     // Start adding known signal names
-    try names.add_signal_name(.io_A8,  "GP-0");
+    try names.add_signal_name(Chip.pins._23.pad(),  "SRAM-A16");
+
+    // CPLD connections to FLASH (address bus + control)
+    try names.add_signal_name(Chip.pins._100.pad(), "FLASH-A0");
+    try names.add_signal_name(Chip.pins._84.pad(),  "FLASH-A1");
+    try names.add_signal_name(Chip.pins._91.pad(),  "FLASH-A2");
+    try names.add_signal_name(Chip.pins._85.pad(),  "FLASH-A3");
+    try names.add_signal_name(Chip.pins._90.pad(),  "FLASH-A4");
+    try names.add_signal_name(Chip.pins._92.pad(),  "FLASH-A5");
+    try names.add_signal_name(Chip.pins._87.pad(),  "FLASH-A6");
+    try names.add_signal_name(Chip.pins._99.pad(),  "FLASH-A7");
+    try names.add_signal_name(Chip.pins._108.pad(), "FLASH-A8");
+    try names.add_signal_name(Chip.pins._116.pad(), "FLASH-A9");
+    try names.add_signal_name(Chip.pins._101.pad(), "FLASH-A10");
+    try names.add_signal_name(Chip.pins._109.pad(), "FLASH-A11");
+    try names.add_signal_name(Chip.pins._103.pad(), "FLASH-A12");
+    try names.add_signal_name(Chip.pins._106.pad(), "FLASH-A13");
+    try names.add_signal_name(Chip.pins._89.pad(),  "FLASH-A14");
+    try names.add_signal_name(Chip.pins._111.pad(), "FLASH-A15");
+    try names.add_signal_name(Chip.pins._80.pad(),  "FLASH-A16");   // This actually an input (A16 and A17 are driven by the bus directly!)
+
+    // Flash CE signal (independent from SRAM!)
+    try names.add_signal_name(Chip.pins._118.pad(),  "FLASH-NCE");
+
+
+    // SRAM signals (TODO)
+    // /CS is directly connected to cart's CS2 (assume CS and CS2 are never simultaneously asserted :P)
+
+    // Some shared signals
+
+    // Flash and SRAM chip /WE are tied toghether!
+    try names.add_signal_name(Chip.pins._119.pad(), "FLASH-SRAM-NWE");
+    // Same for /OE signal
+    try names.add_signal_name(Chip.pins._121.pad(), "FLASH-SRAM-NOE");
+
+//    try names.add_signal_name(Chip.pins._9.pad(),  "GP-0");
     try names.add_signal_name(.io_A10, "GP-1");
     try names.add_signal_name(.io_A12, "GP-2");
     try names.add_signal_name(.io_A14, "GP-3");
